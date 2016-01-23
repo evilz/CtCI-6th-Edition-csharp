8a61ec386e6d4a03b425730bf5cee11812e420d6
Vincent B. <evilznet@gmail.com>
2016-01-23 17:11:17 +0100

    Add UniqueChar problem

---
+++ coding-problems/UniqueChar/StringHelper.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace UniqueChar
+{
+    public static class StringHelper
+    {
+        public static bool HasUniqueChar(this string source)
+        {
+            var existingChar = new bool[char.MaxValue];
+
+            foreach (var c in source)
+            {
+                if (existingChar[c]) return false;
+                existingChar[c] = true;
+            }
+            return true;
+        }
+    }
+
+
+    public class StringHelperTests
+    {
+        [TestCase("abcdefghij",true)]
+        [TestCase("aa",false)]
+        [TestCase("abcdefghijabcdefghij", false)]
+        public void HasUniqueChar_Tests(string source,bool expected)
+        {
+            Assert.That(source.HasUniqueChar(), Is.EqualTo(expected));
+        }
+    }
+}
